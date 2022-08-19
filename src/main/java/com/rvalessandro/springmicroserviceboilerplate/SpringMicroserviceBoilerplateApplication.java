f9e25cae52633b52cfd3b60ba65d24581c46c58a
Raffi Verrel Alessandro <ralessandro939@gmail.com>
2022-08-19 10:08:17 +0700

    Initial Commit

---
+++ src/main/java/com/rvalessandro/springmicroserviceboilerplate/SpringMicroserviceBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.rvalessandro.springmicroserviceboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceBoilerplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMicroserviceBoilerplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rvalessandro/springmicroserviceboilerplate/SpringMicroserviceBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rvalessandro.springmicroserviceboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceBoilerplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
